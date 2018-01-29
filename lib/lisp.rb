4be7b5269765c8d4b763752d7bad3a9757680457
Benjamin Trevor <trevorb@nextcapital.com>
2018-01-29 16:56:19 -0600

    remove class, implement recursive evaluation

--- lib/lisp.rb
+++ lib/lisp.rb
@@ -1,53 +1,59 @@
-class Lisp
-  class ParseError < StandardError; end
+class ParseError < StandardError; end
 
-  class << self
-    def evaluate(input)
-      new(input).evaluate
+def evaluate(input)
+  case input[0]
+  when "'"
+    symbols = get_symbols(input[1..-1])
+    symbols.map{|s| evaluate(s)}
+  when '('
+    symbols = get_symbols(input)
+    apply(symbols)
+  when /\d/
+    if input =~ /^\d*$/
+      input.to_i
+    else
+      raise ParseError.new("can't parse #{input}")
+    end
+  else
+    raise ParseError.new("can't start with #{input[0]}")
+  end
+end
+
+def apply(symbols)
+  return if symbols.empty?
+
+  applied = symbols.dup
+
+  symbols.each_with_index do |s, i|
+    if s.is_a?(Array)
+      applied[i] = apply(s).to_s
     end
   end
 
-  attr_reader :input
-
-  def initialize(input)
-    @input = input
+  case applied.first
+  when '+', '-', '*', '/'
+    applied[1..-1].map{|x| evaluate(x)}.inject(applied.first)
+  else
+    raise ParseError.new('invalid symbol')
   end
+end
 
-  def evaluate
-    case input[0]
-    when "'"
-      symbols = get_symbols(input[1..-1])
-      symbols.map{|s| Lisp.evaluate(s)}
-    when '('
-      symbols = get_symbols(input)
-      apply_function(symbols)
-    when /\d/
-      if input =~ /^\d*$/
-        input.to_i
-      else
-        raise ParseError.new("can't parse #{input}")
-      end
+def get_symbols(input_sexpr)
+  sexpr = input_sexpr.strip
+
+  if !(sexpr.start_with?('(') && sexpr.end_with?(')'))
+    raise ParseError.new('unbalanced parens')
+  else
+    str = sexpr[1..-2]
+    first_paren = str.index('(')
+
+    if first_paren
+      last_paren = str.rindex(')')
+      sub_expression = str[first_paren..last_paren]
+
+      str[0..(first_paren-1)].split + [get_symbols(sub_expression)] + str[(last_paren+1)..-1].split
     else
-      raise ParseError.new("can't start with #{input[0]}")
-    end
-  end
-
-  def apply_function(symbols)
-    return if symbols.empty?
-
-    case symbols.first
-    when '+', '-', '*', '/'
-      symbols[1..-1].map{|x| Lisp.evaluate(x)}.inject(symbols.first)
-    else
-      raise ParseError.new('invalid symbol')
-    end
-  end
-
-  def get_symbols(str)
-    if !(str.start_with?('(') && str.end_with?(')'))
-      raise ParseError.new('unbalanced parens')
-    else
-      str[1..-2].split
+      str.split
     end
   end
 end
--- spec/lisp_spec.rb
+++ spec/lisp_spec.rb
@@ -1,55 +1,54 @@
 require 'spec_helper'
 
-describe Lisp do
-  describe "#evaluate" do
-    context "for valid inputs" do
-      ERROR = 'error!'
+describe "#evaluate" do
+  context "for valid inputs" do
+    ERROR = 'error!'
 
-      {
-        # start with just integers
-        "should evaluate numbers" => ["1", 1],
-        "should evaluate empty list" => ["'()", []],
-        "should evaluate list of numbers" => ["'(1 2 3)", [1, 2, 3]],
-        "should raise an error for lists missing closing parens" => ["'(1 2", ERROR],
+    {
+      # start with just integers
+      "should evaluate numbers" => ["1", 1],
+      "should evaluate empty list" => ["'()", []],
+      "should evaluate list of numbers" => ["'(1 2 3)", [1, 2, 3]],
+      "should raise an error for lists missing closing parens" => ["'(1 2", ERROR],
 
-        "should evaluate null expression" => ["()", nil],
-        "should raise an error for missing close-parens" => ["(", ERROR],
-        "should raise an error for missing open-parens" => [")", ERROR],
-        "should raise an error for unbalanced s-expressions" => ["(+ 1", ERROR],
+      "should evaluate null expression" => ["()", nil],
+      "should raise an error for missing close-parens" => ["(", ERROR],
+      "should raise an error for missing open-parens" => [")", ERROR],
+      "should raise an error for unbalanced s-expressions" => ["(+ 1", ERROR],
 
-        # evaluate functions
-        "should evaluate addition" => ["(+ 6 2)", 8],
-        "should evaluate subtraction" => ["(- 6 2)", 4],
-        "should evaluate multiplication" => ["(* 6 2)", 12],
-        "should evaluate division" => ["(/ 6 2)", 3],
-        "should raise an error for bogus functions" => ["(bogus 1 2 3)", ERROR],
-        "should raise an error for division by zero" => ["(/ 1 0)", ERROR],
+      # evaluate functions
+      "should evaluate addition" => ["(+ 6 2)", 8],
+      "should evaluate subtraction" => ["(- 6 2)", 4],
+      "should evaluate multiplication" => ["(* 6 2)", 12],
+      "should evaluate division" => ["(/ 6 2)", 3],
+      "should raise an error for bogus functions" => ["(bogus 1 2 3)", ERROR],
+      "should raise an error for division by zero" => ["(/ 1 0)", ERROR],
 
-        # stricter parsing
-        "should raise an error for invalid symbols" => ["1asdf", ERROR],
-        "should raise an error for trying to apply non-function" => ["(1 2 3)", ERROR],
+      # stricter parsing
+      "should raise an error for invalid symbols" => ["1asdf", ERROR],
+      "should raise an error for trying to apply non-function" => ["(1 2 3)", ERROR],
 
-        # # recursive evaluation
-        # "recursive evaluation" => ["(+ 6 (- 10 2))", 14],
+      # # recursive evaluation
+      "recursive evaluation" => ["(+ 6 (- 10 2))", 14],
+      "deep recursive evaluation" => ["(+ 6 (- 10 (+ 2 4)))", 10],
 
-        # # add floats
-        # "floats" => ["1.5", 1.5],
-        # "float arithmetic" => ["(+ 1.5 (- 3.6 1.2))", 3.9],
+      # # add floats
+      # "floats" => ["1.5", 1.5],
+      # "float arithmetic" => ["(+ 1.5 (- 3.6 1.2))", 3.9],
 
-        # # add strings
-        # "strings" => ['"asdf"', "asdf"],
-        # "string functions" => ['(concat "as" "df")', "asdf"],
-        # "should raise an error for type mismatch" => ['(+ "asdf" 3)', ERROR],
+      # # add strings
+      # "strings" => ['"asdf"', "asdf"],
+      # "string functions" => ['(concat "as" "df")', "asdf"],
+      # "should raise an error for type mismatch" => ['(+ "asdf" 3)', ERROR],
 
-        # # add bindings
-        # "bindings" => ["(let (x 1) (+ x 2))", 3],
-      }.each do |desc, (expression, result)|
-        it "#{desc}: `#{expression} → #{result}`" do
-          if result == ERROR
-            expect { Lisp.evaluate(expression) }.to raise_error(StandardError)
-          else
-            expect(Lisp.evaluate(expression)).to eq(result)
-          end
+      # # add bindings
+      # "bindings" => ["(let (x 1) (+ x 2))", 3],
+    }.each do |desc, (expression, result)|
+      it "#{desc}: `#{expression} → #{result}`" do
+        if result == ERROR
+          expect { evaluate(expression) }.to raise_error(StandardError)
+        else
+          expect(evaluate(expression)).to eq(result)
         end
       end
     end
