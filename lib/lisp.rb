5bd86e19c5aeb4ff63a805693a7fe8e670cf3b4c
Benjamin Trevor <trevorb@nextcapital.com>
2018-01-29 15:58:31 -0600

    next few specs

--- lib/lisp.rb
+++ lib/lisp.rb
@@ -2,8 +2,8 @@ class Lisp
   class ParseError < StandardError; end
 
   class << self
-    def eval(input)
-      new(input).eval
+    def evaluate(input)
+      new(input).evaluate
     end
   end
 
@@ -13,14 +13,23 @@ class Lisp
     @input = input
   end
 
-  def eval
+  def evaluate
     case input[0]
     when "'"
-      []
+      symbols = get_symbols(input[1..-1])
+      symbols.map{|s| Lisp.evaluate(s)}
     when '('
       nil
     else
       input.to_i
+    end
+  end
+
+  def get_symbols(str)
+    if !(str.start_with?('(') && str.end_with?(')'))
+      raise ParseError
+    else
+      str[1..-2].split
     end
   end
 end
--- spec/lisp_spec.rb
+++ spec/lisp_spec.rb
@@ -1,52 +1,54 @@
 require 'spec_helper'
 
 describe Lisp do
-  describe "#eval" do
+  describe "#evaluate" do
     context "for valid inputs" do
+      ERROR = 'error!'
+
       {
         # start with just integers
-        "numbers" => ["1", 1],
-        "null expression" => ["()", nil],
-        "empty list" => ["'()", []],
-        "list of numbers" => ["'(1 2 3)", [1, 2, 3]],
+        "should evaluate numbers" => ["1", 1],
+        "should evaluate empty list" => ["'()", []],
+        "should evaluate list of numbers" => ["'(1 2 3)", [1, 2, 3]],
+        "should raise an error for lists missing closing parens" => ["'(1 2", ERROR],
+
+        "should evaluate null expression" => ["()", nil],
+        "should raise an error for missing close-parens" => ["(", ERROR],
+        "should raise an error for missing open-parens" => [")", ERROR],
+        "should raise an error for unbalanced s-expressions" => ["(+ 1", ERROR],
 
         # evaluate functions
-        "addition" => ["(+ 6 2)", 8],
-        "subtraction" => ["(- 6 2)", 4],
-        "multiplication" => ["(* 6 2)", 12],
-        "division" => ["(/ 6 2)", 3],
+        "should evaluate addition" => ["(+ 6 2)", 8],
+        "should evaluate subtraction" => ["(- 6 2)", 4],
+        "should evaluate multiplication" => ["(* 6 2)", 12],
+        "should evaluate division" => ["(/ 6 2)", 3],
+        "should raise an error for division by zero" => ["(/ 1 0)", ERROR],
 
-        # recursive evaluation
-        "recursive evaluation" => ["(+ 6 (- 10 2))", 14],
+        # stricter parsing
+        "should raise an error for invalid symbols" => ["1asdf", ERROR],
+        "should raise an error for trying to apply non-function" => ["(1 2 3)", ERROR],
 
-        # add floats
-        "floats" => ["1.5", 1.5],
-        "float arithmetic" => ["(+ 1.5 (- 3.6 1.2))", 3.9],
+        # # recursive evaluation
+        # "recursive evaluation" => ["(+ 6 (- 10 2))", 14],
 
-        # add strings
-        "strings" => ['"asdf"', "asdf"],
-        "string functions" => ['(concat "as" "df")', "asdf"],
+        # # add floats
+        # "floats" => ["1.5", 1.5],
+        # "float arithmetic" => ["(+ 1.5 (- 3.6 1.2))", 3.9],
 
-        # add bindings
-        "bindings" => ["(let (x 1) (+ x 2))", 3],
+        # # add strings
+        # "strings" => ['"asdf"', "asdf"],
+        # "string functions" => ['(concat "as" "df")', "asdf"],
+        # "should raise an error for type mismatch" => ['(+ "asdf" 3)', ERROR],
+
+        # # add bindings
+        # "bindings" => ["(let (x 1) (+ x 2))", 3],
       }.each do |desc, (expression, result)|
-        it "should evaluate #{desc}: `#{expression} → #{result}`" do
-          expect(Lisp.eval(expression)).to eq(result)
-        end
-      end
-    end
-
-    context "for invalid inputs" do
-      {
-        "missing close paren" => "(1",
-        "missing open paren" => "1)",
-        "invalid symbols" => "1asdf",
-        "trying to apply non-function" => "(1 2 3)",
-        "division by zero" => "(/ 1 0)",
-        "type mismatch" => '(+ "asdf" 3)',
-      }.each do |desc, expression|
-        it "should raise an error for #{desc}" do
-          expect { Lisp.eval(expression) }.to raise_error(Lisp::ParseError)
+        it "#{desc}: `#{expression} → #{result}`" do
+          if result == ERROR
+            expect { Lisp.evaluate(expression) }.to raise_error(Lisp::ParseError)
+          else
+            expect(Lisp.evaluate(expression)).to eq(result)
+          end
         end
       end
     end
